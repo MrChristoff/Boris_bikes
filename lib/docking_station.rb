de6ae1204dd7d9246c1d30956562a21e9d428591
jinimcoroneo <jinim1983@gmail.com>
2016-09-29 16:47:40 +0100

    challenge 15 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,14 +8,24 @@ class DockingStation
   end
 
   def release_bike
-    raise StandardError if @bikes_docked.size < 1
+    raise StandardError if empty?
     Bike.new
   end
 
   def dock_bike(name)
-    raise StandardError if @bikes_docked.size > 19
+    raise StandardError if full?
     @name = name
     @bikes_docked << name
+  end
+
+  private
+
+  def full?
+    @bikes_docked.size >= 20 ? true : false
+  end
+
+  def empty?
+    @bikes_docked.size < 1 ? true : false
   end
 
 end
