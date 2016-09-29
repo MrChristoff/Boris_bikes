07e5f33f16aa68e0e64b5961ea63195c115544c1
jinimcoroneo <jinim1983@gmail.com>
2016-09-29 12:25:55 +0100

    completed exercise 11 !

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,18 @@
 require_relative 'bike.rb'
 
 class DockingStation
+  attr_reader :bikes_docked
+
+  def initialize
+    @bikes_docked = []
+  end
 
   def release_bike
     Bike.new
   end
-end
 
+  def dock_bike(name)
+    @name = name
+  end
+
+end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,18 +2,26 @@ require 'docking_station'
 
 describe DockingStation do
 
-  before do 
-    @station = DockingStation.new  
+  before do
+    @station = DockingStation.new
   end
- 
-  it "should respond to release bike" do  
-    expect(@station).to respond_to(:release_bike) 
-  end 
+
+  it "should respond to release bike" do
+    expect(@station).to respond_to(:release_bike)
+  end
 
   it "should return a working bike" do
     bike = @station.release_bike
     expect(bike.working?).to eq (true)
   end
-  
-end
 
+  it "should check that the dock_bike method exists" do
+    expect(@station).to respond_to(:dock_bike)
+  end
+
+  it "passes when the arguments match" do
+    bike = @station.dock_bike("name")
+    expect(bike).to eql ("name")
+  end
+
+end
